4b5d7cf68fceed06f457d4ca9c729376fd6e7c38
baobao-2008 <baovnqth06015@gmail.com>
2026-04-07 12:53:05 +0700

    hàm Sercirity mở cho xem giao diện điều hướng login, logout, bảo mật truy cập

---
+++ src/main/java/vn/fpoly/assignmentjava202/Config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package vn.fpoly.assignmentjava202.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.AuthProvider;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf-> csrf.disable())// tạm tời tắt scurity
+        .authorizeHttpRequests(auth->auth//Thieets lập các quy tắc
+                .requestMatchers("/home/**","/css/**","/images/**","/auth/login").permitAll() // cho phép các trang này không cần kiểm tra qua lơp bảo mật để đăng nhập
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()// Để thực hiện mua hàng hay thanh toán cần phải đăng nhập
+        )
+                .formLogin(login->login
+                        .loginPage("/auth/login")//Đường dẫn trang login
+                        .loginProcessingUrl("/auth/login-process")// url xư lý khi nhân nút đăng nhập
+                        .defaultSuccessUrl("/home/index",true)// Thành công
+                        .failureUrl("/auth/login?error=true")// Thât bại
+                        .permitAll()
+                )
+                .logout(logout->logout
+                        .logoutUrl("/auth/logout")
+                        .logoutSuccessUrl("/home/index")
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
