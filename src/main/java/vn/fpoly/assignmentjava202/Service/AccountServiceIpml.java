9f32b28a3f9e2dd2589bc9b8308afbe551b72a42
baobao-2008 <baovnqth06015@gmail.com>
2026-04-10 12:22:29 +0700

    Hoàn thiện giao diện account và product

--- src/main/java/vn/fpoly/assignmentjava202/Service/AccountServiceIpml.java
+++ src/main/java/vn/fpoly/assignmentjava202/Service/AccountServiceIpml.java
@@ -3,7 +3,6 @@ package vn.fpoly.assignmentjava202.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import vn.fpoly.assignmentjava202.DAO.AccountDAO;
-import vn.fpoly.assignmentjava202.DAO.OrderDAO;
 import vn.fpoly.assignmentjava202.Entity.Accounts;
 
 import java.util.List;
