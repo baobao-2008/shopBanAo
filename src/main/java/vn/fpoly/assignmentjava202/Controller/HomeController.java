3eb412327f5c6f65d5c532189ed665d3143aaf9d
baobao-2008 <baovnqth06015@gmail.com>
2026-04-10 20:16:04 +0700

    Xong login khi ấn vòa thoát khi xong đăng nhập thì dổi thành logout

--- src/main/java/vn/fpoly/assignmentjava202/Controller/HomeController.java
+++ src/main/java/vn/fpoly/assignmentjava202/Controller/HomeController.java
@@ -5,14 +5,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import vn.fpoly.assignmentjava202.DAO.AccountDAO;
 import vn.fpoly.assignmentjava202.Entity.Products;
 import vn.fpoly.assignmentjava202.Service.ProductService;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
+
+    @Autowired
+    AccountDAO accountDAO;
+
     @Autowired
     ProductService productService;
 
