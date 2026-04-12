b1933d4f128bfbf134f7169d1cb6a0391d73d806
baobao-2008 <baovnqth06015@gmail.com>
2026-04-12 23:31:45 +0700

    done tìm kiếm theo tên

--- src/main/java/vn/fpoly/assignmentjava202/Controller/HomeController.java
+++ src/main/java/vn/fpoly/assignmentjava202/Controller/HomeController.java
@@ -5,11 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import vn.fpoly.assignmentjava202.DAO.AccountDAO;
+import vn.fpoly.assignmentjava202.DAO.ProductDAO;
 import vn.fpoly.assignmentjava202.Entity.Products;
 import vn.fpoly.assignmentjava202.Service.ProductService;
 
-import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -22,10 +23,19 @@ public class HomeController {
     @Autowired
     ProductService productService;
 
+    @Autowired
+    ProductDAO productDAO;
+
     @GetMapping("/index")
     public String index(Model model) {
         List<Products> products = productService.getAllProducts();
         model.addAttribute("products", products);
         return "trang-chu";
+    }
+
+    @GetMapping("/search-by-category")
+    public String searchByCategory(Model model, @RequestParam("name")String name) {
+       model.addAttribute("products", productDAO.findByCategoryNameContainingIgnoreCase(name));
+       return "trang-chu";
     }
 }
--- src/main/java/vn/fpoly/assignmentjava202/DAO/ProductDAO.java
+++ src/main/java/vn/fpoly/assignmentjava202/DAO/ProductDAO.java
@@ -1,8 +1,12 @@
 package vn.fpoly.assignmentjava202.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import vn.fpoly.assignmentjava202.Entity.Products;
+
+import java.util.List;
 
 public interface ProductDAO extends JpaRepository<Products, Integer> {
 
+    List<Products> findByCategoryNameContainingIgnoreCase(String name);
 }
